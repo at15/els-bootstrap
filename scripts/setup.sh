09ca1d47e5f031dbe5844637429cf4032dcb7244
at15 <at15@outlook.com>
2016-02-26 15:53:27 +0800

    Finish the setup script for ik and els

--- scripts/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.2.0/elasticsearch-2.2.0.tar.gz
-tar -zxvf ../bundle/elasticsearch-2.2.0.tar.gz -C ../bundle
