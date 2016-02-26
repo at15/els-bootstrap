71e3e609683f3a1d2519043da4d16e3fc1a6d8bb
at15 <at15@outlook.com>
2016-02-26 16:19:36 +0800

    Fix the async problem in test script
    have to wait for a while before els deal with data and index, query
    right away will have 0 hit

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,14 +2,21 @@
 
 echo "test if ik is working, make sure you have elasticsearch running!"
 
+
+echo "delete old index" 
+curl -XDELETE http://localhost:9200/ik
+
+# NOTE: delete is async, so the result is not expected .... sleep may help
+sleep 1
+
 echo "create index called ik"
-curl -XPUT http://localhost:9200/ik
+curl -XPUT http://localhost:9200/ik?pretty
 
 echo "create index mapping"
-curl -XPOST http://localhost:9200/ik/fulltext/_mapping -d'
+curl -XPOST http://localhost:9200/ik/fulltext/_mapping?pretty -d'
 {
     "fulltext": {
-             "_all": {
+            "_all": {
             "analyzer": "ik_max_word",
             "search_analyzer": "ik_max_word",
             "term_vector": "no",
@@ -41,7 +48,10 @@ curl -XPOST http://localhost:9200/ik/fulltext/2?pretty -d'
 curl -XPOST http://localhost:9200/ik/fulltext/3?pretty -d'
 {"content":"阿姨最萌了"}
 '
+# the indexing process is async as well ... near real time
+sleep 1
 
+# FIXME: hit is empty
 # TODO: why pre_tags is an array
 echo "searching"
 curl -XPOST http://localhost:9200/ik/fulltext/_search?pretty  -d'
